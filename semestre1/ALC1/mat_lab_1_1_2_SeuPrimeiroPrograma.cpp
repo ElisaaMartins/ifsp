48602f4e58247981fe802ab20942f519e84a1ce4
ElisaaMartins <liaparecidamartinsoliveira@gmail.com>
2025-08-22 11:27:18 -0300

    primeiros códigos do curso

---
+++ semestre1/ALC1/mat_lab_1_1_2_SeuPrimeiroPrograma.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std:
+
+int Main (void) {
+
+    // faça o programa cumprimentá-lo de uma forma calorosa e agradável
+    cout << "Ola! Eu sou seu primeiro programa!\n"; // adicionando \n (nova linha)
+    cout << endl;
+
+    // duplique a saudação para dar as boas-vindas a mais de uma pessoa
+    cout << Seja bem vindo, José! Eu sou seu primeiro programa!:
+    cout << endl;
+    cout << "Seja bem vinda Maria! Eu sou zeu primeiro programa!"
+
+    return 0;
+}
